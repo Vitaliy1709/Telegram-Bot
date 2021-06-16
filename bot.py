141322a06f8f65c84e9d762f3ffb4206abae4a97
unknown <autoparts76@ukr.net>
2021-06-16 17:15:42 +0200

    Add project telegram-bot

---
+++ bot.py
@@ -0,0 +1,19 @@
+import telebot
+
+bot = telebot.TeleBot('Telegram_Bot_Token')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понял, а поздороваться?')
+
+
+bot.polling(none_stop=True)
